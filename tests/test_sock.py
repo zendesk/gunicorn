aadbb52742c6da0eadcaf6f098aa514e474d7b4c
Randall Leeds <randall@bleeds.info>
2016-07-18 22:52:23 -0700

    Refactor socket activation and fd inheritance
    Track the use of systemd socket activation and gunicorn socket inheritance
    in the arbiter. Unify the logic of creating gunicorn sockets from each of
    these sources to always use the socket name to determine the type rather
    than checking the configured addresses. The configured addresses are only
    used when there is no inheritance from systemd or a parent arbiter.
    
    Fix #1298

--- tests/test_sock.py
+++ tests/test_sock.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -
+#
+# This file is part of gunicorn released under the MIT license.
+# See the NOTICE for more information.
+
 try:
     import unittest.mock as mock
 except ImportError:
@@ -6,10 +11,22 @@ except ImportError:
 from gunicorn import sock
 
 
-@mock.patch('os.getpid')
 @mock.patch('os.unlink')
-@mock.patch('socket.fromfd')
-def test_unix_socket_close_unlink(fromfd, unlink, getpid):
-    gsock = sock.UnixSocket('test.sock', mock.Mock(), mock.Mock(), mock.Mock())
-    gsock.close()
-    unlink.assert_called_with("test.sock")
+def test_close_sockets(unlink):
+    listener1 = mock.Mock()
+    listener1.getsockname.return_value = ('127.0.0.1', '80')
+    listener2 = mock.Mock()
+    listener2.getsockname.return_value = '/var/run/test.sock'
+    sock.close_sockets([listener1, listener2])
+    listener1.close.assert_called_with()
+    listener2.close.assert_called_with()
+    unlink.assert_called_once_with('/var/run/test.sock')
+
+
+@mock.patch('os.unlink')
+def test_close_sockets_without_unlink(unlink):
+    listener1 = mock.Mock()
+    listener1.getsockname.return_value = '/var/run/test.sock'
+    sock.close_sockets([listener1], False)
+    listener1.close.assert_called_with()
+    assert not unlink.called, 'unlink should not have been called'
